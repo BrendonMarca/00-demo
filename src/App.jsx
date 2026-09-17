1c7681ee718b8c95fcc7ecd6b8c57e5a06dfbed4
Brendon <B90423479@gmail.com>
2026-09-17 16:58:50 -0300

    atm medio

--- src/App.jsx
+++ src/App.jsx
@@ -78,24 +78,145 @@ function Usodecelular() {
 
   alert("A chance de uso de celular é: " + porc.toFixed(2) + "%");
 }
+function frete(){
+
+let peso = Number(prompt("qual o peso do frete?"));
+let distancia = Number(prompt("qual a distancia do frete?"));
+let volume = Number(prompt("qual o volume do frete?"));
+
+let total =15+(2*peso)+(0.05*distancia)+(10*volume)
+  alert("O valor total do frete é: R$" + total.toFixed(2));
+
+}
+function Dona() {
+
+  let bt = Number(prompt("qual o valor Bruto?"));
+  let pp = Number(prompt("qual o valor de premiações e presentes ?"));
+  let cs = Number(prompt("qual o valor das comissões?"));
+  
+  let lucro = bt - pp - cs;
+  alert("O lucro da Dona é: R$" + lucro.toFixed(2));
+}
+function capitao() {
+  let gasto = Number(prompt("Quanto foi gasto em suprimentos e mercadorias para operar seu navio;?")); 
+  let fatura = Number(prompt("Quanto foi o faturamento em venda de ingressos?"));
+  let fatuitens = Number(prompt("Quando foi o faturamento em venda de ítens?"));
+  let lucro = fatura + fatuitens - gasto;
+  let percentual = (lucro / gasto) * 100;
+
+  alert("O lucro do capitão é: R$" + lucro.toFixed(2) + " e o percentual de lucro é: " + percentual.toFixed(2) + "%");
+
+}
+
+function Sarranosmanos() {
+  let shows = Number(prompt("Quantos shows?"));
+  let unidade = Number(prompt("Qual o valor por bomba?"));
+
+  let quantidade = shows * 7;
+  let total = quantidade * unidade;
+
+  alert("Você precisa de " + quantidade + " bombas.");
+  alert("O total é: R$ " + total.toFixed(2));
+}
+function mano22() {
+  let salario = Number(prompt("Quanto ganhou?"));
+  let despesas = Number(prompt("Quanto gastou com todas as despesas?"));
+  let lucro = salario - despesas;
+
+  alert("O só sobrou: R$" + lucro.toFixed(2));
+}
+function romero() {
+  let valor = Number(prompt("Qual era o valor antes?"));
+  let resposta = valor * 3;
+
+  alert("O valor agora é: R$" + resposta)
 
 
+}
+function petshop() {
+  let peso = Number(prompt("Quantos quilos?"));
+  let valor = peso / 100;
 
-  return (
+  alert("O valor da ração é: R$" + valor.toFixed(2));
+}
+function calcularchurras(){
+   let pessoas = Number(prompt("Quantas pessoas ao todo:"));
+   let adultosCarne = Number(prompt("Quantos adultos comem carne:"));
+   let criancasCarne = Number(prompt("Quantas crianças comem carne:"));
+   let pessoascerveja = Number(prompt("quantas pessoas bebem cerveja:"))
+   let pessoasCe = pessoascerveja
+   let nivel = prompt("digite o nivel da sua compra \nleve\nmedio\nmonstro")
+   let carne = ""
+   let cerveja = ""
+   let agua = ""
+   let refri = ""
+   let carvao = ""
+   let sal = ""
+
+   if(nivel == "leve"){
+   carne = (adultosCarne * 0.5) + (criancasCarne * 0.25); 
+   cerveja = pessoasCe*1
+   agua = pessoas*0.5
+   refri = pessoas*0.2
+   carvao = pessoas*0.5
+   sal = pessoas*0.25
+
+   alert("o quantia final de tudo sera de\n"+carne+"KG de carne\n"+cerveja+"L de cerveja\n"+agua+"L de água\n"+refri+"L de refri\n"+carvao+"KG de carvão\n"+sal+"KG de sal")
+   }
+   else if(nivel == "medio"){
+   carne = (adultosCarne * 1) + (criancasCarne * 0.5);
+   cerveja = pessoasCe*2
+   agua = pessoas*1
+   refri = pessoas*0.5
+   carvao = pessoas*1
+   sal = pessoas*0.5
+
+   alert("o quantia final de tudo sera de\n"+carne+"KG de carne\n"+cerveja+"L de cerveja\n"+agua+"L de água\n"+refri+"L de refri\n"+carvao+"KG de carvão\n"+sal+"KG de sal")
+   }
+   else if(nivel == "monstro"){
+   carne = (adultosCarne * 2) + (criancasCarne * 1);
+   cerveja = pessoasCe*4
+   agua = pessoas*2
+   refri = pessoas*1
+   carvao = pessoas*2
+   sal = pessoas*1
+
+   alert("o quantia final de tudo sera de\n"+carne+"KG de carne\n"+cerveja+"L de cerveja\n"+agua+"L de água\n"+refri+"L de refri\n"+carvao+"KG de carvão\n"+sal+"KG de sal")
+   }
+    let racha = prompt("Deseja calcular o racha?\nsim\nnao");
+
+    if(racha == "sim"){
+        let valor = Number(prompt("Qual foi o valor total gasto?"));
+        let pessoasRacha = Number(prompt("Quantas pessoas vao dividir?"));
+
+        let valorI = valor / pessoasRacha;
+
+        alert("Cada pessoa vai pagar R$" + valorI.toFixed(2));
+    }
+ }
+
+return (
     <div className="cont-app">
       <h1>Trampando com React</h1>
       
       <h2>Exercícios supimpas </h2>
 
-       <button onClick={calcularPontos}>campeonato</button>
-         <button onClick={trocarSapatos}> trocas pé pequeno</button>
-           <button onClick={calcularDevs}>devs</button>
-             <button onClick={quantidadedelaranjas}>calculo de laranjas</button>
-               <button onClick={custosDaIgreja}>custos da igreja</button>
-                  <button onClick={calcularSalario}>JUNIN</button>
-                     <button onClick={pesodetransporte}>transporte</button>
-                        <button onClick={Usodecelular}>celular</button>  
-
+      <button onClick={calcularPontos}>campeonato</button>
+      <button onClick={trocarSapatos}> trocas pé pequeno</button>
+      <button onClick={calcularDevs}>devs</button>
+      <button onClick={quantidadedelaranjas}>calculo de laranjas</button>
+      <button onClick={custosDaIgreja}>custos da igreja</button>
+      <button onClick={calcularSalario}>JUNIN</button>
+      <button onClick={pesodetransporte}>transporte</button>
+      <button onClick={Usodecelular}>celular</button>  
+      <button onClick={frete}>frete</button>  
+      <button onClick={Dona}>Judas</button>    
+      <button onClick={capitao}>navio</button>
+      <button onClick={Sarranosmanos}>shows</button>  
+      <button onClick={mano22}>pespesas do mano</button>
+      <button onClick={romero}>o romero</button>
+      <button onClick={petshop}>os pet </button>
+      <button onClick={calcularchurras}></button>
       <hr />
     </div>
   )
